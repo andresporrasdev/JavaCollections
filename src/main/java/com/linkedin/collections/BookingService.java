8c8e144c5436322ae19a5e20ea700c2a3f456541
Andres Porras <camilop1730@gmail.com>
2024-05-08 19:34:56 -0400

    Finished exercise using Map Collections and methods

--- src/main/java/com/linkedin/collections/BookingService.java
+++ src/main/java/com/linkedin/collections/BookingService.java
@@ -13,12 +13,19 @@ public class BookingService {
 		 * 1. The provided Guest is placed in the bookings Map and
 		 * associated with the provided room, only if no other guest
 		 * is associated with the room.
-		 * 
+		 *
 		 * Returns a boolean that indicates if the Guest was
 		 * successfully placed in the room.
 		 */
-		
-		return false;
+			// Check if the room is already booked
+		if (bookings.containsKey(room)) {
+			// If the room is already booked, return false
+			return false;
+		} else {
+			// If the room is not booked, add the guest to the bookings map and return true
+			bookings.put(room, guest);
+			return true;
+		}
 	}
 
 	public double totalRevenue() {
@@ -27,7 +34,9 @@ public class BookingService {
 		 * 2. Returns a double that totals the rate of each Room booked
 		 * in the bookings Map.
 		 */
-		return 0;
+		return bookings.keySet().stream()
+				.mapToDouble(Room::getRate)
+				.sum();
 	}
 	
 	public Map<Room, Guest> getBookings() {
